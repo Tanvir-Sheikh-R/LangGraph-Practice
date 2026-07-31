98f0f88406176552cb9006d6339c07a39e623f3b
Tanvir-Sheikh-R <tanvirsheikh0177@gmail.com>
2026-07-31 20:59:39 +0600

    streaming added

--- chat_app_frontend.py
+++ chat_app_frontend.py
@@ -10,20 +10,27 @@ if 'message' not in st.session_state:
 st.title('Chat App', text_alignment='center')
 st.subheader('Hi, Whats your agenda today?', text_alignment='center')
 
+for messagees in st.session_state.message:
+    with st.chat_message(messagees["role"]):
+        st.write(messagees['msg'])
+
 user_input = st.chat_input("Type here")
 
 if user_input:
     st.session_state.message.append({'role': 'user', 'msg': user_input})
 
-    config = {'configurable': {'thread_id': '1'}}
-    response = chat.invoke({'message': [HumanMessage(user_input)]}, config=config)
-
-    st.session_state.message.append({'role': 'assistant', 'msg':  response['message'][-1].content})
-
-
-for messagees in st.session_state.message:
-    with st.chat_message(messagees["role"]):
-        st.write(messagees["msg"])
+    with st.chat_message('user'):
+            st.write(user_input)
+    
+    with st.chat_message('assistant'):
+        response = st.write_stream(
+            message_chunk.content for message_chunk, metadata in chat.stream(
+                {'message': [HumanMessage(user_input)]}, 
+                config={'configurable': {'thread_id': '1'}}, 
+                stream_mode='messages'
+            ))
+    st.session_state.message.append({'role': 'assistant', 'msg':  response})
+            
 
 print()
 print(st.session_state)
